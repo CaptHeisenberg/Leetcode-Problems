341b5f620b51e51d017fe7d395b782ef3c451298
Atharva Thombare <128704865+Atharva1210@users.noreply.github.com>
2025-03-20 08:42:59 +0530

    Time: 255 ms (17.39%), Space: 91.9 MB (88.41%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
@@ -1,71 +1,38 @@
+class UnionFind:
+    def __init__(self, N):
+        self.root = list(range(N))
+        self.Size = [1]*N
+
+    def Find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.Find(self.root[x])  # path compression
+        return self.root[x]
+
+    def Union(self, x, y):
+        x = self.Find(x)
+        y = self.Find(y)
+        if x==y: return False
+
+        if self.Size[x] > self.Size[y]:
+            self.Size[x] += self.Size[y]
+            self.root[y]=x
+        else:
+            self.Size[y] += self.Size[x]
+            self.root[x]=y
+        return True
 class Solution:
-    def minimumCost(self, n, edges, queries):
-        # Create the adjacency list of the graph
-        adj_list = [[] for _ in range(n)]
-        for edge in edges:
-            adj_list[edge[0]].append((edge[1], edge[2]))
-            adj_list[edge[1]].append((edge[0], edge[2]))
-
-        visited = [False] * n
-
-        # Array to store the component ID of each node
-        components = [0] * n
-        component_cost = []
-
-        component_id = 0
-
-        # Perform BFS for each unvisited node to identify components and calculate their costs
-        for node in range(n):
-            if not visited[node]:
-                # Get the component cost and mark all nodes in the component
-                component_cost.append(
-                    self._get_component_cost(
-                        node, adj_list, visited, components, component_id
-                    )
-                )
-                component_id += 1
-
-        result = []
-        for query in queries:
-            start, end = query
-
-            if components[start] == components[end]:
-                # If they are in the same component, return the precomputed cost for the component
-                result.append(component_cost[components[start]])
-            else:
-                # If they are in different components, return -1
-                result.append(-1)
-
-        return result
-
-    # Helper function to calculate the cost of a component using BFS
-    def _get_component_cost(
-        self, source, adj_list, visited, components, component_id
-    ):
-        nodes_queue = deque()
-
-        # Initialize the component cost to the number that has only 1s in its binary representation
-        component_cost = -1
-
-        nodes_queue.append(source)
-        visited[source] = True
-
-        # Perform BFS to explore the component and calculate the cost
-        while nodes_queue:
-            node = nodes_queue.popleft()
-
-            # Mark the node as part of the current component
-            components[node] = component_id
-
-            # Explore all neighbors of the current node
-            for neighbor, weight in adj_list[node]:
-                # Update the component cost by performing a bitwise AND of the edge weights
-                component_cost &= weight
-
-                # If the neighbor hasn't been visited, mark it as visited and add it to the queue
-                if visited[neighbor]:
-                    continue
-                visited[neighbor] = True
-                nodes_queue.append(neighbor)
-
-        return component_cost
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        graph=UnionFind(n)
+        cost=[0xFFFFFFFF]*n
+        for u, v, w in edges:
+            wt=cost[graph.Find(u)] & cost[graph.Find(v)] & w
+            graph.Union(u, v)
+            rt=graph.Find(u)
+            cost[rt]&=wt
+        ans=[-1]*(len(query))
+        for i, (s, t) in enumerate(query):
+            rootS=graph.Find(s)
+            if rootS==graph.Find(t):
+                ans[i]=cost[rootS]
+        return ans
+        
