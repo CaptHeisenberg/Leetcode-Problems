e0c412fc00c4196999573006d171cddaecae9875
Atharva Thombare <128704865+CaptHeisenberg@users.noreply.github.com>
2025-06-06 11:37:08 +0530

    Time: 19 ms (6.27%), Space: 19.4 MB (51.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,8 +3,8 @@ class Solution:
         count = {}
         for num in nums:
             if num in count:
-                count[num] +=1
+                count[num]+=1
             else:
-                count[num] = 1
+                count[num]=1
             if count[num]>len(nums)//2:
                 return num
