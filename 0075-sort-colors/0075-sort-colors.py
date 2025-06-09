f476ebbbafb83f9ae4996f79460f420484ef2099
Atharva Thombare <128704865+CaptHeisenberg@users.noreply.github.com>
2025-06-09 21:43:37 +0530

    Time: 1 ms (15.13%), Space: 17.9 MB (12.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,33 +1,8 @@
-import random
-
 class Solution:
-    def sortColors(self, nums):
-        
-        def randomized_quick_sort(arr, low, high):
-            if low < high:
-                pivot_index = randomized_partition(arr, low, high)
-                randomized_quick_sort(arr, low, pivot_index - 1)
-                randomized_quick_sort(arr, pivot_index + 1, high)
-        
-        def partition(arr, low, high):
-            pivot = arr[high]
-            i = low - 1
-            for j in range(low, high):
-                if arr[j] < pivot:
-                    i += 1
-                    arr[i], arr[j] = arr[j], arr[i]
-            arr[i + 1], arr[high] = arr[high], arr[i + 1]
-            return i + 1
-        
-        def randomized_partition(arr, low, high):
-            pivot_index = random.randint(low, high)
-            arr[high], arr[pivot_index] = arr[pivot_index], arr[high]
-            return partition(arr, low, high)
-
-        randomized_quick_sort(nums, 0, len(nums) - 1)
-
-# Example Usage:
-solution = Solution()
-nums = [2, 0, 2, 1, 1, 0]
-solution.sortColors(nums)
-print(nums)
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        for i in range(0, n):
+            for j in range(0, n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
