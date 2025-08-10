9188e318db5836f16fae7f2649108e3ab5402898
Atharva Thombare <128704865+CaptHeisenberg@users.noreply.github.com>
2025-08-10 09:15:13 +0530

    Time: 1435 ms (20.23%), Space: 40.8 MB (17.56%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.py
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -1,11 +1,17 @@
+from math import log2
+from itertools import permutations
+
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        def count_digits(x):
-            return ''.join(sorted(str(x)))
-
-        target = count_digits(n)
-        
-        for i in range(31):
-            if count_digits(1 << i) == target:
+        s = str(n)
+        seen = set()
+        for perm in permutations(s):
+            if perm[0] == '0':
+                continue
+            num = int(''.join(perm))
+            if num in seen:
+                continue
+            seen.add(num)
+            if num > 0 and (log2(num)).is_integer():
                 return True
         return False
